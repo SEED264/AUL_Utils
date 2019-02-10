06aa6bbdf659f46c3b831f9f217067f76437bc4e
SEED264 <seedx264@gmail.com>
2019-02-11 08:32:04 +0900

    Add Pixel_RGBA struct

--- include/aut/AUT_Type.h
+++ include/aut/AUT_Type.h
@@ -1,4 +1,13 @@
 #ifndef _AUL_UTILS_INCLUDE_AUT_AUL_TYPE_H_
 #define _AUL_UTILS_INCLUDE_AUT_AUL_TYPE_H_
 
+// unsigned longの代わりのメモリ上の配置が同じになる構造体
+// メモリの配置順の都合上、内部の変数の順番はBGRAでなくてはならない
+struct Pixel_RGBA {
+	unsigned char b = 0;
+	unsigned char g = 0;
+	unsigned char r = 0;
+	unsigned char a = 0;
+};
+
 #endif // _AUL_UTILS_INCLUDE_AUT_AUL_TYPE_H_
