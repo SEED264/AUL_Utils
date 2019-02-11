bbf7f69415c8a44c4cd6b7e5645650b90443d963
SEED264 <seedx264@gmail.com>
2019-02-11 09:43:05 +0900

    Add Size_2D struct

--- include/aut/AUT_Type.h
+++ include/aut/AUT_Type.h
@@ -18,6 +18,15 @@ namespace aut{
             Pixel_RGBA(0, 0, 0, 0);
         }
     };
+
+    // 画像のサイズ等に使う構造体
+    struct Size_2D{
+        unsigned int w, h;
+        Size_2D(unsigned int aw, unsigned int ah):w(aw), h(ah) {}
+        Size_2D() {
+            Size_2D(0, 0);
+        }
+    };
 }
 
 #endif // _AUL_UTILS_INCLUDE_AUT_AUL_TYPE_H_
