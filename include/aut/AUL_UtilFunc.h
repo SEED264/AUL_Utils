b10b5ef0e950c3cff0ba007f3fe7802f5fdd70c3
SEED264 <seedx264@gmail.com>
2019-02-11 11:19:20 +0900

    Add functions

--- include/aut/AUL_UtilFunc.h
+++ include/aut/AUL_UtilFunc.h
@@ -1,4 +1,19 @@
 #ifndef _AUL_UTILS_INCLUDE_AUT_AUL_UTILFUNC_H_
 #define _AUL_UTILS_INCLUDE_AUT_AUL_UTILFUNC_H_
 
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include "aut/AUL_Type.h"
+
+namespace aut {
+    inline double toRadian(double deg) {
+        return deg * M_PI / 180;
+    }
+    inline double toDegree(double rad) {
+        return rad * 180 / M_PI;
+    }
+}
+
+
 #endif // _AUL_UTILS_INCLUDE_AUT_AUL_UTILFUNC_H_
