f2016314785b04c3b797d7c7bee5ca941bcc96ea
SEED264 <seedx264@gmail.com>
2019-02-11 09:43:47 +0900

    Add includes

--- include/aut/AUL_Utils.h
+++ include/aut/AUL_Utils.h
@@ -1,4 +1,7 @@
 #ifndef _AUL_UTILS_INCLUDE_AUT_AUL_UTILS_H_
 #define _AUL_UTILS_INCLUDE_AUT_AUL_UTILS_H_
 
+#include "aut/AUT_Type.h"
+#include "aut/AUL_UtilFunc.h"
+
 #endif // _AUL_UTILS_INCLUDE_AUT_AUL_UTILS_H_
