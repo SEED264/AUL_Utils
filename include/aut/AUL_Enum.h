82cd12bde10fae32b90bd763b6918d04993443e0
SEED264 <seedx264@gmail.com>
2019-02-11 10:46:02 +0900

    Add AUL_Enum.h

---
+++ include/aut/AUL_Enum.h
@@ -0,0 +1,12 @@
+#ifndef _AUL_UTILS_INCLUDE_AUT_AUL_ENUM_H_
+#define _AUL_UTILS_INCLUDE_AUT_AUL_ENUM_H_
+
+namespace aut {
+    enum getpixeldata_op {
+        none = 0,
+        work = 1,
+        alloc = 2
+    };
+}
+
+#endif // _AUL_UTILS_INCLUDE_AUT_AUL_ENUM_H_
