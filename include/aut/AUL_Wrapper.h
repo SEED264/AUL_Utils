5c0c2f17189a0679b76c1fb9cbc950de6281ee2b
SEED264 <seedx264@gmail.com>
2019-02-21 01:49:31 +0900

    Add load function

--- include/aut/AUL_Wrapper.h
+++ include/aut/AUL_Wrapper.h
@@ -27,6 +27,8 @@ namespace aut {
     int draw(lua_State *L, double ox = 0, double oy = 0, double oz = 0, double zoom = 1, double alpha = 1, double rx = 0, double ry = 0, double rz = 0);
     int drawpoly(lua_State *L, double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3,
                  double u0 = 0, double v0 = 0, double u1 = USHRT_MAX, double v1 = 0, double u2 = USHRT_MAX, double v2 = USHRT_MAX, double u3 = 0, double v3 = USHRT_MAX, double alpha = 1);
+    template <typename... Parms>
+    int load(lua_State *L, Parms... parms);
     int getpixeldata(lua_State *L, Pixel_RGBA **out_data, Size_2D *out_size, const std::vector<std::string> &option = std::vector<std::string>());
     int getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *out_h, const std::vector<std::string> &option = std::vector<std::string>());
     int putpixeldata(lua_State *L, Pixel_RGBA *data);
@@ -87,6 +89,14 @@ int aut::drawpoly(lua_State *L, double x0, double y0, double z0, double x1, doub
              double u0, double v0, double u1, double v1, double u2, double v2, double u3, double v3, double alpha) {
     getAULFunc(L, "drawpoly");
     size_t pushedNum = setArgs(L, x0, y0, z0, x1, y1, z1, x2, y2, z2, x3, y3, z3, u0, v0, u1, v1, u2, v2, u3, v3, alpha);
+    lua_call(L, pushedNum, 0);
+    return 0;
+}
+
+template <typename... Parms>
+int aut::load(lua_State *L, Parms... parms) {
+    aut::getAULFunc(L, "load");
+    size_t pushedNum = setArgs(L, parms...);
     lua_call(L, pushedNum, 0);
     return 0;
 }
