2d0e5c0361db75132c63bb14221cdc44abffdd5f
SEED264 <seedx264@gmail.com>
2019-02-21 12:19:56 +0900

    Change wrapper function's return type to void

--- include/aut/AUL_Wrapper.h
+++ include/aut/AUL_Wrapper.h
@@ -23,21 +23,21 @@ namespace aut {
     size_t setArgs(lua_State *L, T value, Parms... parms);
 
     template <typename... Parms>
-    int effect(lua_State *L, Parms... parms);
-    int draw(lua_State *L, double ox = 0, double oy = 0, double oz = 0, double zoom = 1, double alpha = 1, double rx = 0, double ry = 0, double rz = 0);
-    int drawpoly(lua_State *L, double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3,
+    void effect(lua_State *L, Parms... parms);
+    void draw(lua_State *L, double ox = 0, double oy = 0, double oz = 0, double zoom = 1, double alpha = 1, double rx = 0, double ry = 0, double rz = 0);
+    void drawpoly(lua_State *L, double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3,
                  double u0 = 0, double v0 = 0, double u1 = USHRT_MAX, double v1 = 0, double u2 = USHRT_MAX, double v2 = USHRT_MAX, double u3 = 0, double v3 = USHRT_MAX, double alpha = 1);
     template <typename... Parms>
-    int load(lua_State *L, Parms... parms);
+    void load(lua_State *L, Parms... parms);
     template <typename... Parms>
-    int setfont(lua_State *L, const std::string &name, double size, Parms... parms);
+    void setfont(lua_State *L, const std::string &name, double size, Parms... parms);
     template <typename... Parms>
     double rand(lua_State *L, double st_num, double ed_num, Parms... parms);
     template <typename... Parms>
-    int setoption(lua_State *L, const std::string &name, Parms... parms);
-    int getpixeldata(lua_State *L, Pixel_RGBA **out_data, Size_2D *out_size, const std::vector<std::string> &option = std::vector<std::string>());
-    int getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *out_h, const std::vector<std::string> &option = std::vector<std::string>());
-    int putpixeldata(lua_State *L, Pixel_RGBA *data);
+    void setoption(lua_State *L, const std::string &name, Parms... parms);
+    void getpixeldata(lua_State *L, Pixel_RGBA **out_data, Size_2D *out_size, const std::vector<std::string> &option = std::vector<std::string>());
+    void getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *out_h, const std::vector<std::string> &option = std::vector<std::string>());
+    void putpixeldata(lua_State *L, Pixel_RGBA *data);
 }
 
 void aut::getAULFunc(lua_State *L, const std::string &funcName) {
@@ -77,21 +77,21 @@ size_t aut::setArgs(lua_State *L, T value, Parms... parms) {
 }
 
 template <typename... Parms>
-int aut::effect(lua_State *L, Parms... parms) {
+void aut::effect(lua_State *L, Parms... parms) {
     aut::getAULFunc(L, "effect");
     size_t pushedNum = setArgs(L, parms...);
     lua_call(L, pushedNum, 0);
     return 0;
 }
 
-int aut::draw(lua_State *L, double ox, double oy, double oz, double zoom, double alpha, double rx, double ry, double rz) {
+void aut::draw(lua_State *L, double ox, double oy, double oz, double zoom, double alpha, double rx, double ry, double rz) {
     getAULFunc(L, "draw");
     size_t pushedNum = setArgs(L, ox, oy, oz, zoom, alpha, rx, ry, rz);
     lua_call(L, pushedNum, 0);
     return 0;
 }
 
-int aut::drawpoly(lua_State *L, double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3,
+void aut::drawpoly(lua_State *L, double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3,
              double u0, double v0, double u1, double v1, double u2, double v2, double u3, double v3, double alpha) {
     getAULFunc(L, "drawpoly");
     size_t pushedNum = setArgs(L, x0, y0, z0, x1, y1, z1, x2, y2, z2, x3, y3, z3, u0, v0, u1, v1, u2, v2, u3, v3, alpha);
@@ -100,19 +100,17 @@ int aut::drawpoly(lua_State *L, double x0, double y0, double z0, double x1, doub
 }
 
 template <typename... Parms>
-int aut::load(lua_State *L, Parms... parms) {
+void aut::load(lua_State *L, Parms... parms) {
     aut::getAULFunc(L, "load");
     size_t pushedNum = setArgs(L, parms...);
     lua_call(L, pushedNum, 0);
-    return 0;
 }
 
 template <typename... Parms>
-int aut::setfont(lua_State *L, const std::string &name, double size, Parms... parms) {
+void aut::setfont(lua_State *L, const std::string &name, double size, Parms... parms) {
     aut::getAULFunc(L, "setfont");
     size_t pushedNum = setArgs(L, name, size, parms...);
     lua_call(L, pushedNum, 0);
-    return 0;
 }
 
 template <typename... Parms>
@@ -124,18 +122,17 @@ double aut::rand(lua_State *L, double st_num, double ed_num, Parms... parms) {
 }
 
 template <typename... Parms>
-int aut::setoption(lua_State *L, const std::string &name, Parms... parms) {
+void aut::setoption(lua_State *L, const std::string &name, Parms... parms) {
     getAULFunc(L, "setoption");
     size_t pushedNum = setArgs(L, name, parms...);
     lua_call(L, pushedNum, 0);
-    return 0;
 }
 
-int aut::getpixeldata(lua_State *L, Pixel_RGBA **out_data, Size_2D *out_size, const std::vector<std::string> &option) {
-    return getpixeldata(L, out_data, &out_size->w, &out_size->h, option);
+void aut::getpixeldata(lua_State *L, Pixel_RGBA **out_data, Size_2D *out_size, const std::vector<std::string> &option) {
+    getpixeldata(L, out_data, &out_size->w, &out_size->h, option);
 }
 
-int aut::getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *out_h, const std::vector<std::string> &option) {
+void aut::getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *out_h, const std::vector<std::string> &option) {
     getAULFunc(L, "getpixeldata");
     int argnum = 0;
     for(size_t i = 0; i < option.size(); i++) {
@@ -148,14 +145,12 @@ int aut::getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *ou
     *out_data = (Pixel_RGBA*)lua_touserdata(L, -3);
     lua_pop(L, 3);
     if (*out_data == nullptr) return -1;
-    return 0;
 }
 
-int aut::putpixeldata(lua_State *L, Pixel_RGBA *data) {
+void aut::putpixeldata(lua_State *L, Pixel_RGBA *data) {
     getAULFunc(L, "putpixeldata");
     lua_pushlightuserdata(L, data);
     lua_call(L, 1, 0);
-    return 0;
 }
 
 #endif // _AUL_UTILS_INCLUDE_AUT_AUL_WRAPPER_H_
