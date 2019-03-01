50462bd76adf4d56130a657b99f1d4d45923cb39
SEED264 <seedx264@gmail.com>
2019-03-02 02:43:49 +0900

    Fix setanchor

--- include/aut/AUL_Wrapper.h
+++ include/aut/AUL_Wrapper.h
@@ -51,7 +51,7 @@ namespace aut {
     bool getoption_multi_object(lua_State *L);
     template<typename T, typename... Parms>
     lua_Number getvalue(lua_State *L, T target, Parms... parms);
-    template<typename T, typename... Parms>
+    template<typename... Parms>
     lua_Integer setanchor(lua_State *L, const std::string &name, lua_Integer num, Parms... parms);
     void getpixeldata(lua_State *L, Pixel_RGBA **out_data, Size_2D *out_size, const std::vector<std::string> &option = std::vector<std::string>());
     void getpixeldata(lua_State *L, Pixel_RGBA **out_data, uint *out_w, uint *out_h, const std::vector<std::string> &option = std::vector<std::string>());
@@ -276,8 +276,8 @@ lua_Number aut::getvalue(lua_State *L, T target, Parms... parms) {
     return ret;
 }
 
-template<typename T, typename... Parms>
-lua_Integer setanchor(lua_State *L, const std::string &name, lua_Integer num, Parms... parms) {
+template<typename... Parms>
+lua_Integer aut::setanchor(lua_State *L, const std::string &name, lua_Integer num, Parms... parms) {
     getAULFunc(L, "setanchor");
     size_t pushedNum = setArgs(L, name, num, parms...);
     lua_call(L, pushedNum, 1);
