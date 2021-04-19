762666a96516a012f5db3969bae842d44e8bba0a
SEED264 <seedx264@gmail.com>
2021-04-19 22:18:45 +0900

    Edit license year

--- include/aut/AUL_Type.h
+++ include/aut/AUL_Type.h
@@ -3,7 +3,7 @@
  *
  * The MIT License
  *
- * Copyright (c) 2019 SEED264
+ * Copyright (c) 2019, 2021 SEED264
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
